6ac73bbb02875ce6aa4b67ba5516b284f9d33b73
Niels <nutralmedia@gmail.com>
2021-05-18 12:14:19 +0200

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,10 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
 const dirApp = path.join(__dirname, 'app')
+const dirImages = path.join(__dirname, 'images')
 const dirShared = path.join(__dirname, 'shared')
 const dirStyles = path.join(__dirname, 'styles')
+const dirVideos = path.join(__dirname, 'videos')
 const dirNode = path.join(__dirname, 'node_modules')
 
 module.exports = {
@@ -22,6 +24,8 @@ module.exports = {
             dirApp,
             dirStyles,
             dirShared,
+            dirImages,
+            dirVideos,
             dirNode
         ]
     },
@@ -38,8 +42,49 @@ module.exports = {
                     to: ''
                 }
             ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
         })
-    ]
+    ],
 
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: ''
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    }
+                ]
+
+            },
+
+            {
+                test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+                loader: 'file-loader'
+            }
+        ]
+    }
 
 }
