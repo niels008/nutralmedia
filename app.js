3b6d47cd9d1c49460d8253aae11696b7f44111db
Niels <nutralmedia@gmail.com>
2021-05-23 20:39:12 +0200

    added views and pug

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 1337
 
+app.set('views', path.join(__dirname, 'app'))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(port, () => {
