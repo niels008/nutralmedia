4f3d04957b5ed6be2e0e1e0b37c377ac1be42c16
Niels <nutralmedia@gmail.com>
2021-05-23 22:00:42 +0200

    adding first stuff for navigation and changed npm run build

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const app = express()
 const path = require('path')
 const port = 1337
 
-app.set('views', path.join(__dirname, 'app'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    meta: {
+      data: {
+        title: 'Nutral',
+        description: 'Metadata description.'
+      }
+    }
+  })
 })
 
 app.listen(port, () => {
