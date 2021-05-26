a97b6c5a4c3086d4e8c75d51a2304408abe6c323
Niels <nutralmedia@gmail.com>
2021-05-26 13:06:17 +0200

    app js implement prismic

--- app.js
+++ app.js
@@ -5,6 +5,29 @@ const app = express()
 const path = require('path')
 const port = 1337
 
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
+
+const handleLinkResolver = doc => {
+  return '/'
+}
+
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver
+  }
+  res.locals.PrismicDOM = PrismicDOM
+  next()
+})
+
+const initApi = req => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req
+  })
+}
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
@@ -16,8 +39,21 @@ app.get('/collections/', (req, res) => {
   res.render('pages/collections')
 })
 
-app.get('/about', (req, res) => {
-  res.render('pages/about')
+app.get('/about', async (req, res) => {
+  initApi(req).then(api => {
+    api.query(Prismic.Predicates.any('document.type', ['about', 'metadata'])).then((response) => {
+      const { results } = response
+      const [meta, about] = results
+      console.log(results)
+
+      console.log(meta, about)
+
+      res.render('pages/about', {
+        about,
+        meta
+      })
+    })
+  })
 })
 
 app.get('/detail/:uid', (req, res) => {
