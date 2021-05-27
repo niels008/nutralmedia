b92654ee531e5343c34fad5e8d5e2eceb8230755
Niels <nutralmedia@gmail.com>
2021-05-27 21:03:34 +0200

    first base without error handling

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 1337
+const port = 3000
 
 const Prismic = require('@prismicio/client')
 const PrismicDOM = require('prismic-dom')
+
+const initApi = req => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req
+  })
+}
 
 const handleLinkResolver = doc => {
   return '/'
@@ -17,39 +24,29 @@ app.use((req, res, next) => {
     endpoint: process.env.PRISMIC_ENDPOINT,
     linkResolver: handleLinkResolver
   }
+
   res.locals.PrismicDOM = PrismicDOM
+
   next()
 })
-
-const initApi = req => {
-  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
-    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
-    req
-  })
-}
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.render('pages/home')
-})
-
-app.get('/collections/', (req, res) => {
-  res.render('pages/collections')
 })
 
 app.get('/about', async (req, res) => {
   initApi(req).then(api => {
-    api.query(Prismic.Predicates.any('document.type', ['about', 'meta'])).then((response) => {
+    api.query(Prismic.Predicates.any('document.type', ['about', 'meta'])).then(response => {
+      console.log(response)
       const { results } = response
       const [about, meta] = results
-      console.log(`
-      meta
-      data
-      here
-      `)
-      console.log(results)
+
+      about.data.body.gallery.forEach(media => {
+        console.log(media)
+      })
 
       res.render('pages/about', {
         about,
@@ -59,7 +56,11 @@ app.get('/about', async (req, res) => {
   })
 })
 
-app.get('/detail/:uid', (req, res) => {
+app.get('/collections/', (req, res) => {
+  res.render('pages/collections')
+})
+
+app.get('/detail/:uid', async (req, res) => {
   res.render('pages/detail')
 })
 
