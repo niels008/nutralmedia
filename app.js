935f36897006be7f39613f87c169876904a9d279
Niels <nutralmedia@gmail.com>
2021-06-03 15:28:52 +0200

    implementing scss stucture and fonts

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(methodOverride())
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
